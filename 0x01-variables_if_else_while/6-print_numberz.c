e730acb0f3b72fe61013034d4df36aa714535d96
leandreAlly <tuyambazeleandre@gmail.com>
2022-09-09 17:55:23 +0000

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+		putchar("%d", a);
+	putchar('\n');
+	return (0);
+}
